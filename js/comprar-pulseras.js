d590679aeb0d09a6801b4e5bc60c912273f9e9e7
Andrés <marlon_rivera95@hotmail.com>
2022-12-19 11:45:43 -0500

    subida de p54111

---
+++ js/comprar-pulseras.js
@@ -0,0 +1,223 @@
+// PULSERAS
+document.querySelector("#submit13").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let ref13 = document.querySelector("#ref13").textContent;
+let n13 = document.querySelector("#n13").textContent;
+let precio13 = document.querySelector("#precio13").textContent;
+let material13= document.querySelector("#mate13").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Glamour By Ramos_*%0A%0A
+*Sección pulseras*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${ref13}%0A%0A
+*Nombre del producto:*%0D
+${n13}%0A%0A
+*Material:*%0D
+${material13}%0A%0A
+*Precio:*%0D
+${precio13}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submit14").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let ref14 = document.querySelector("#ref14").textContent;
+let n14 = document.querySelector("#n14").textContent;
+let precio14 = document.querySelector("#precio14").textContent;
+let material14= document.querySelector("#mate14").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Glamour By Ramos_*%0A%0A
+*Sección pulseras*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${ref14}%0A%0A
+*Nombre del producto:*%0D
+${n14}%0A%0A
+*Material:*%0D
+${material14}%0A%0A
+*Precio:*%0D
+${precio14}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submit15").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let ref15 = document.querySelector("#ref15").textContent;
+let n15 = document.querySelector("#n15").textContent;
+let precio15 = document.querySelector("#precio15").textContent;
+let material15= document.querySelector("#mate15").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Glamour By Ramos_*%0A%0A
+*Sección pulseras*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${ref15}%0A%0A
+*Nombre del producto:*%0D
+${n15}%0A%0A
+*Material:*%0D
+${material15}%0A%0A
+*Precio:*%0D
+${precio15}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submit16").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let ref16 = document.querySelector("#ref16").textContent;
+let n16 = document.querySelector("#n16").textContent;
+let precio16 = document.querySelector("#precio16").textContent;
+let material16= document.querySelector("#mate16").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Glamour By Ramos_*%0A%0A
+*Sección pulseras*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${ref16}%0A%0A
+*Nombre del producto:*%0D
+${n16}%0A%0A
+*Material:*%0D
+${material16}%0A%0A
+*Precio:*%0D
+${precio16}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submit5").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let ref5 = document.querySelector("#ref5").textContent;
+let n5 = document.querySelector("#n5").textContent;
+let precio5 = document.querySelector("#precio5").textContent;
+let material5= document.querySelector("#mate5").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Glamour By Ramos_*%0A%0A
+*Sección conjuntos*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${ref5}%0A%0A
+*Nombre del producto:*%0D
+${n5}%0A%0A
+*Material:*%0D
+${material5}%0A%0A
+*Precio:*%0D
+${precio5}%0A%0A`;
+
+window.open(url);    
+});
+
+document.querySelector("#submit6").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let ref6 = document.querySelector("#ref6").textContent;
+let n6 = document.querySelector("#n6").textContent;
+let precio6 = document.querySelector("#precio6").textContent;
+let material6= document.querySelector("#mate6").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Glamour By Ramos_*%0A%0A
+*Sección conjuntos*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${ref6}%0A%0A
+*Nombre del producto:*%0D
+${n6}%0A%0A
+*Material:*%0D
+${material6}%0A%0A
+*Precio:*%0D
+${precio6}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submit7").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let ref7 = document.querySelector("#ref7").textContent;
+let n7 = document.querySelector("#n7").textContent;
+let precio7 = document.querySelector("#precio7").textContent;
+let material7= document.querySelector("#mate7").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Glamour By Ramos_*%0A%0A
+*Sección conjuntos*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${ref7}%0A%0A
+*Nombre del producto:*%0D
+${n7}%0A%0A
+*Material:*%0D
+${material7}%0A%0A
+*Precio:*%0D
+${precio7}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submit8").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let ref8 = document.querySelector("#ref8").textContent;
+let n8 = document.querySelector("#n8").textContent;
+let precio8 = document.querySelector("#precio8").textContent;
+let material8= document.querySelector("#mate8").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Glamour By Ramos_*%0A%0A
+*Sección conjuntos*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${ref8}%0A%0A
+*Nombre del producto:*%0D
+${n8}%0A%0A
+*Material:*%0D
+${material8}%0A%0A
+*Precio:*%0D
+${precio8}%0A%0A`;
+
+window.open(url);    
+});
+    
